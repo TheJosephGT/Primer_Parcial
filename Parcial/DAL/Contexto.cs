6bb8a950ed72b65dcff45c9d9483021279e8174e
JosephGT <joseph2002610@gmail.com>
2023-02-13 18:48:30 -0400

    Creado el contexto, junto con los framework porque pensaba que ya lo tenia jeje

---
+++ Parcial/DAL/Contexto.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
+public class Contexto : DbContext{
+
+    public DbSet<Libros> Libros {get;set;}
+
+    public Contexto (DbContextOptions<Contexto> options): base(options){}
+
+}
